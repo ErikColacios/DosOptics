70937a8f420f444450f16884575d287aba0f9011
Erik Colacios García <erik.colacios@ekon.es>
2025-02-13 17:20:53 +0100

    pagina nosaltres

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,6 @@ import tailwind from '@astrojs/tailwind';
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  site: 'https://ErikColacios.github.com',
-  base: 'DosOptics',
+  // site: 'https://ErikColacios.github.com',
+  // base: 'DosOptics',
 });
